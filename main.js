ceb6620abaa67865474127312aefa1963fe50f86
Muhammad Muzammil Ali <119755415+M1muzammil@users.noreply.github.com>
2023-04-16 02:37:58 -0700

    to be continue

---
+++ main.js
@@ -0,0 +1,142 @@
+ function first (event) {
+{event.preventDefault()}
+
+let first = ""
+let number = Math.random(first)*6
+number=Number.parseInt(number)
+if (number===0){
+document.querySelector("#first").innerHTML= number +1 }
+    else if (number === 1) {
+
+        document.querySelector('#first').innerHTML = number }
+
+     else if (number === 2) {
+
+        document.querySelector('#first').innerHTML = number
+
+    } 
+    else if (number === 3) {
+
+        document.querySelector('#first').innerHTML = number
+
+    } 
+    else if (number === 4) {
+
+        document.querySelector('#first').innerHTML = number
+
+    } 
+    else {
+
+        document.querySelector('#first').innerHTML = number
+
+    }
+
+
+    }
+    function seconde (event) {
+        {event.preventDefault()}
+        
+        let seconde = ""
+        let number = Math.random(seconde)*6
+        number=Number.parseInt(number)
+        if (number===0){
+        document.querySelector("#seconde").innerHTML= number +1 }
+            else if (number === 1) {
+        
+                document.querySelector('#seconde').innerHTML = number }
+        
+             else if (number === 2) {
+        
+                document.querySelector('#seconde').innerHTML = number
+        
+            } 
+            else if (number === 3) {
+        
+                document.querySelector('#seconde').innerHTML = number
+        
+            } 
+            else if (number === 4) {
+        
+                document.querySelector('#seconde').innerHTML = number
+        
+            } 
+            else {
+        
+                document.querySelector('#seconde').innerHTML = number
+        
+            }
+        
+        
+            }
+            function third (event) {
+                {event.preventDefault()}
+                
+                let third = ""
+                let number = Math.random(third)*6
+                number=Number.parseInt(number)
+                if (number===0){
+                document.querySelector("#third").innerHTML= number +1 }
+                    else if (number === 1) {
+                
+                        document.querySelector('#third').innerHTML = number }
+                
+                     else if (number === 2) {
+                
+                        document.querySelector('#third').innerHTML = number
+                
+                    } 
+                    else if (number === 3) {
+                
+                        document.querySelector('#third').innerHTML = number
+                
+                    } 
+                    else if (number === 4) {
+                
+                        document.querySelector('#third').innerHTML = number
+                
+                    } 
+                    else {
+                
+                        document.querySelector('#third').innerHTML = number
+                
+                    }
+                
+                
+                    }
+                    function fourth (event) {
+                        {event.preventDefault()}
+                        
+                        let fourth = ""
+                        let number = Math.random(fourth)*6
+                        number=Number.parseInt(number)
+                        if (number===0){
+                        document.querySelector("#fourth").innerHTML= number +1 }
+                            else if (number === 1) {
+                        
+                                document.querySelector('#fourth').innerHTML = number }
+                        
+                             else if (number === 2) {
+                        
+                                document.querySelector('#fourth').innerHTML = number
+                        
+                            } 
+                            else if (number === 3) {
+                        
+                                document.querySelector('#fourth').innerHTML = number
+                        
+                            } 
+                            else if (number === 4) {
+                        
+                                document.querySelector('#fourth').innerHTML = number
+                        
+                            } 
+                            else {
+                        
+                                document.querySelector('#fourth').innerHTML = number
+                        
+                            }
+                        
+                        
+                            }
+
+
